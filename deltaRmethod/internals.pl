b644237aef4289490eff04b5b7f22fcabe2764f9
ElizabethForest <elizabeth.forest@my.jcu.edu.au>
2020-07-31 15:10:46 +1000

    Initial commit with data from trello card

---
+++ deltaRmethod/internals.pl
@@ -0,0 +1,38 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:deltar/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:scatter/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sitedistribution/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:withwithout/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:all/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:location/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:no_dc/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:no_do/;
+$ref_files{$key} = "$dir".q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ deltaRmethod/labels.pl
@@ -0,0 +1,81 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:deltar/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:scatter/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sitedistribution/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:withwithout/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:all/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:location/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:no_dc/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:no_do/;
+$external_labels{$key} = "$URL/" . q|deltaRmethod.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# labels from external_latex_labels array.
+
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:deltar/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:scatter/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sitedistribution/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:withwithout/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:all/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:location/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:no_dc/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:no_do/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+1;
+
