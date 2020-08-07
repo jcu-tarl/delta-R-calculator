cb9fcf762337aed1ea37f9fcdf4cc5f145acbd95
ElizabethForest <elizabeth.forest@my.jcu.edu.au>
2020-08-07 16:15:33 +1000

    Cleaned up router.js a little

--- js/router.js
+++ js/router.js
@@ -1,51 +1,66 @@
 page.base('/delta-R-calculator');
-page('/about', about);
-page('/', get_coords_and_calculate);
+page('/', go_to_page);
 page('*', not_found);
 page(get_coords_and_calculate);
 page();
 
 var loaded = '';
 
-get_coords();
-
-// TODO: have the JS get the values?
 function calculate_deltaR() {
     calculate(document.getElementById('latitude').value, document.getElementById('longitude').value,
         document.getElementById('d13C').value, document.getElementById('d180').value);
 }
 
-calculate_deltaR();
+function calculate_query_param_deltaR() {
+    get_coords();
+    calculate_deltaR();
+}
 
-async function get_coords_and_calculate(x) {
-    if (loaded !== 'calculator') {
-        document.getElementsByClassName('container')[0].innerHTML = await (await fetch('pages/calculator.html')).text();
-        loaded = 'calculator';
-    }
-
-    console.log(x);
-    var y = document.getElementById('img-ismap');
-    console.log(y);
-
-    try {
-        get_coords();
-        calculate_deltaR();
-    } catch (e) {
-        console.log("caught exception");
+function go_to_page(ctx) {
+    if (ctx.hash === 'about') {
+        about();
+    } else {
+        get_coords_and_calculate(ctx);
     }
 }
 
-async function about() {
-    if (loaded !== 'about') {
-        document.getElementsByClassName('container')[0].innerHTML = await (await fetch('pages/about.html')).text();
-        loaded = 'about';
+function get_coords_and_calculate(ctx) {
+    if (loaded !== 'calculator') {
+        loaded = 'calculator';
+        load_html_file('pages/calculator.html', calculate_query_param_deltaR);
+    } else {
+        calculate_query_param_deltaR();
     }
-    console.log("in about");
+}
+
+function about() {
+    if (loaded !== 'about') {
+        loaded = 'about';
+        load_html_file('pages/about.html');
+    }
 }
 
 function not_found() {
     if (loaded !== 'not-found') {
-        document.getElementsByClassName('container')[0].innerHTML = "<h1>Page not Found</h1>";
         loaded = 'not-found';
+        get_container().innerHTML = "<h1>Page not Found</h1>";
     }
+}
+
+function load_html_file(file_name, do_after) {
+    let container = get_container();
+    fetch_html(file_name).then(html => {
+        container.innerHTML = html;
+        if (do_after !== undefined) {
+            do_after()
+        }
+    });
+}
+
+async function fetch_html(file_name) {
+    return (await fetch(file_name)).text();
+}
+
+function get_container() {
+    return document.getElementsByClassName('container')[0]
 }
