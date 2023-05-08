5926be190264564db41f0dca723410c3f9e9d520
damienogrady <damien.ogrady@my.jcu.edu.au>
2023-05-09 09:35:12 +1000

    Changed method

--- js/router.js
+++ js/router.js
@@ -6,14 +6,49 @@ page();
 
 var loaded = '';
 
-function calculate_deltaR() {
-    calculate(document.getElementById('latitude').value, document.getElementById('longitude').value,
-        document.getElementById('d13C').value, document.getElementById('d18O').value);
+proj4.defs("EPSG:3577","+proj=aea +lat_0=0 +lon_0=132 +lat_1=-18 +lat_2=-36 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs");
+
+function get_coords(xy){
+    const dX = 0.056355275878;
+    const dY = -0.056406113854;
+    const X = xy[1] * dX + 108.453169373447;
+    const Y = xy[2] * dY - 6.075723287185;
+    document.getElementById('longitude').value = X.toFixed(6);
+    document.getElementById('latitude').value = Y.toFixed(6);
+}
+
+function updateResult(result){
+    document.getElementById("result").innerHTML=""
+            + "<div class='alert alert-success' role='alert'>&Delta;R: " + dr +"</div>";
+}
+
+function calculate_deltaR(){
+    var dr = dr_from_longlat(document.getElementById('longitude').value, document.getElementById('latitude').value);
+    updateResult(dr);
 }
 
 function calculate_query_param_deltaR() {
-    get_coords();
-    calculate_deltaR();
+	const queryString = window.location;
+	const re = /\?(\d+),(\d+)/;
+	var xy = re.exec(queryString);
+    get_coords(xy);
+    dr = deltar(xy[1],xy[2]);
+    updateResult(dr);
+}
+
+function dr_from_longlat(long, lat){
+    en = proj4('EPSG:4326','EPSG:3577',[long, lat]);
+    loc = en_to_rowcol(en[0],en[1]);
+    return deltar(loc[0],loc[1]);
+}
+
+function deltar(x,y){
+    var dr = 999, sigma = 999;
+    if (x > 0 && x < dr_pos.cols && y > 0 && y < dr_pos.rows){
+        dr = dr_vals[y][x];
+        sigma = sigma_vals[y][x];
+    }
+	return dr == 999 ? "Out of bounds" : dr + "±" + sigma;
 }
 
 function go_to_page(ctx) {
@@ -21,15 +56,6 @@ function go_to_page(ctx) {
         about();
     } else {
         get_coords_and_calculate(ctx);
-    }
-}
-
-function get_coords_and_calculate(ctx) {
-    if (loaded !== 'calculator') {
-        loaded = 'calculator';
-        load_html_file('pages/calculator.html', calculate_query_param_deltaR);
-    } else {
-        calculate_query_param_deltaR();
     }
 }
 
@@ -55,6 +81,85 @@ function load_html_file(file_name, do_after) {
             do_after()
         }
     });
+}
+
+function get_coords_and_calculate(ctx) {
+    if (loaded !== 'calculator') {
+        loaded = 'calculator';
+        load_html_file('pages/calculator.html', calculate_query_param_deltaR);
+    } else {
+        calculate_query_param_deltaR();
+    }
+}
+
+async function fetch_html(file_name) {
+    return (await fetch(file_name)).text();
+}
+
+function get_container() {
+    return document.getElementsByClassName('container')[0]
+}
+
+
+function calculate_query_param_deltaR() {
+    get_coords();
+    calculate_deltaR();
+}
+
+function dr_from_longlat(long, lat){
+    en = proj4('EPSG:4326','EPSG:3577',[long, lat]);
+    loc = en_to_rowcol(en[0],en[1]);
+    return deltar(loc[0],loc[1]);
+}
+
+function deltar(x,y){
+    var dr = 999, sigma = 999;
+    if (x > 0 && x < dr_pos.cols && y > 0 && y < dr_pos.rows){
+        dr = dr_vals[y][x];
+        sigma = sigma_vals[y][x];
+    }
+	return dr == 999 ? "Out of bounds" : dr + "±" + sigma;
+}
+
+function go_to_page(ctx) {
+    if (ctx.hash === 'about') {
+        about();
+    } else {
+        get_coords_and_calculate(ctx);
+    }
+}
+
+function about() {
+    if (loaded !== 'about') {
+        loaded = 'about';
+        load_html_file('pages/about.html');
+    }
+}
+
+function not_found() {
+    if (loaded !== 'not-found') {
+        loaded = 'not-found';
+        get_container().innerHTML = "<h1>Page not Found</h1>";
+    }
+}
+
+function load_html_file(file_name, do_after) {
+    let container = get_container();
+    fetch_html(file_name).then(html => {
+        container.innerHTML = html;
+        if (do_after !== undefined) {
+            do_after()
+        }
+    });
+}
+
+function get_coords_and_calculate(ctx) {
+    if (loaded !== 'calculator') {
+        loaded = 'calculator';
+        load_html_file('pages/calculator.html', calculate_query_param_deltaR);
+    } else {
+        calculate_query_param_deltaR();
+    }
 }
 
 async function fetch_html(file_name) {
